847d9a4b2509af1f2edb1d6601960cc77c23d71d
Sebastian Orellana <orellanasebastian2002@gmail.com>
2024-08-25 13:48:04 -0300

    inicio repo

---
+++ script.js
@@ -0,0 +1,9 @@
+const proyectoHover = document.querySelector(".proyecto-hover");
+
+const proyectosCard = document.querySelectorAll(".proyectos-card");
+
+// proyectosCard.forEach(card => {
+//     card.addEventListener("mouseover", e => {
+//         console.log(e.target);
+//     });
+// })
